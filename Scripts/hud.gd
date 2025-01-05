1ea02521aabb5913e6fe2a74242a8530f00c51fc
L30 <93826052+L30HP@users.noreply.github.com>
2025-01-05 13:46:29 +0100

    yeye

--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -11,5 +11,15 @@ func _ready():
 func _process(delta):
 	pass
 
+func get_star_count():
+	return min(ceil($ProgressBar.value/33.333), 3)
+
 func set_progressbar(procent):
+	# Vis stjerner
+	var stars = $Stars.get_children()
+	for n in range(stars.size()):
+		stars[n].hide()
+	for n in range(get_star_count()):
+		stars[n].show()
+
 	$ProgressBar.value = procent
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,5 +1,7 @@
 extends Node3D
-var points = 0
+
+@export_file("*.tscn") var next_level
+
 # Called when the node enters the scene tree for the first time.
 var active_checkpoint: Node3D
 
@@ -7,23 +9,39 @@ func _ready():
 	for Checkpoint in $Map/Checkpoints.get_children():
 		Checkpoint.hide()
 	#show_checkpoint()
-		
+	update_points()
+
+
+func crates_visible():
+	var count = 0 
+	for crate in $Map/Crates.get_children():
+		if crate.visible:
+			count += 1
+	return count
+
 func show_checkpoint():
 	var i = randi_range(0, $Map/Checkpoints.get_children().size() - 1)
 	var Checkpoint = $Map/Checkpoints.get_children()[i]
 	Checkpoint.show()
 
 func update_points():
-	$HUD/Points.text = str(points)
+	$HUD/Points.text = str(Game.points)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if active_checkpoint and active_checkpoint.time_left > 0:
+	if active_checkpoint and active_checkpoint.time_left > -delta:
 		#var procent = $VehicleController2/VehicleRigidBody.speed * 3.6
 		var progress = (100.0*active_checkpoint.time_left)/active_checkpoint.total_time
 		$HUD.set_progressbar(progress)
-		if progress < 0:
+		if progress <= 0:
 			$HUD/ProgressBar.hide()
+			$HUD/Stars.hide()
+			
+	if $VehicleController2/VehicleRigidBody.speed > 3.0:
+		$Intro.hide()
+	elif not $Intro.visible:
+		$Intro.show()
+		$Intro/Restart.show()
 
 func _on_checkpoint_checkpoint_entered(checkpoint: Node3D) -> void:
 	if active_checkpoint:
@@ -33,13 +51,21 @@ func _on_checkpoint_checkpoint_entered(checkpoint: Node3D) -> void:
 	active_checkpoint = checkpoint
 
 	$HUD/ProgressBar.show()
+	$HUD/Stars.show()
 	$VehicleController2/VehicleRigidBody/Crate2.show()
 
 func _on_easy_1_checkpoint_won(checkpoint: Node3D) -> void:
-	if checkpoint.time_left > 0:
-		points += 1
-		update_points()
+	Game.points += $HUD.get_star_count()
+	update_points()
 
 	active_checkpoint = null
 	$HUD/ProgressBar.hide()
+	$HUD/Stars.hide()
 	$VehicleController2/VehicleRigidBody/Crate2.hide()
+
+	if crates_visible() <= 0:
+		get_tree().change_scene_to_file(next_level)
+
+
+func _on_restart_pressed() -> void:
+	pass # Replace with function body.
